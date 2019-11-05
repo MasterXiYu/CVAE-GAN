0f40e3aadb1cbb66f2088545607bf010085e8709
yixu <1532940772@qq.com>
2019-11-05 21:16:23 +0800

    save

--- read_data/read_img.py
+++ read_data/read_img.py
@@ -7,7 +7,11 @@
 @Time    :   4/11/2019 8:29 PM
 '''
 import os
+import cv2
 import numpy as np
+
+def Normalization(data):
+	return data/255.0
 
 
 def get_file(file_dir):  # file_dir="/home1/yixu/yixu_project/CVAE-GAN/download_script/download"
@@ -18,10 +22,13 @@ def get_file(file_dir):  # file_dir="/home1/yixu/yixu_project/CVAE-GAN/download_
 		face_128_dir = os.path.join(face_dir, "face_128*128")
 		for file in os.listdir(face_128_dir):
 			face_file = os.path.join(face_128_dir, file)
-			class_train.append(face_file)  # img as data
+			img = cv2.imread(face_file)
+			img_array = np.array(img)
+			img_array = Normalization(img_array)
+			class_train.append(img_array)  # img as data
 			lable_train.append(path)  # dir as lable
 	# let us do not care label first
-	temp = np.array([class_train])
-	np.random.shuffle(temp)
-	image_list = list(temp)
-	return image_list
+
+	np.random.shuffle(class_train)
+	temp = np.array(class_train) # trun to numpy.data
+	return temp
